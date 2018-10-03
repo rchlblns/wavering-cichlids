270f0f0196b9408bdec0dd18ddf38636083fa11f
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-10-03 16:53:46 -0500

    set up submitBtn event listener

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,10 +1,27 @@
 //function for paralax
 $(document).ready(function(){
-    $('.parallax').parallax();
-  });
+  $('.parallax').parallax();
+});
 //function for slide navbar
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-  //drop down
-  $('.dropdown-trigger').dropdown();
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+//drop down
+$('.dropdown-trigger').dropdown();
+
+// "finish" button click submit form and store variables to local storage
+$("#submitBtn").on("click", function() {
+  // grab text from 1) starting address and save to local storage
+  var startLocation = $("#userAddress").val().trim();
+  localStorage.setItem("startLocation", startLocation);
+  // grabe value from range slider 2) radius and save to local storage
+  var radius = $("#driveRadius").val();
+  localStorage.setItem("radius", radius);
+  // run function that uses Google API
+  // initMap();
+});
+
+function initMap() {
+  console.log("Inside initMap function.");
+}
+
