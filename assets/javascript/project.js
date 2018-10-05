d82907a68b050617697127175e3f9d3f26b96aca
MattsPoche <MatthewsPoche@comcast.net>
2018-10-04 19:04:22 -0500

    added some functionality to the Finnish button

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -16,6 +16,22 @@ $(document).ready(function(){
     
   }
 
+  $("#finish-button").on("click", function(event){
+    event.preventDefault();
+    console.log("BUTTON TEST");
+    var question1ID = $("#question-option1").attr("data-target");
+    var question2ID = $("#question-option2").attr("data-target");
+    user.questionAnswers.push($("#"+question1ID).attr("value"));
+    console.log($("#"+question1ID).attr("value"));
+    user.questionAnswers.push($("#"+question2ID).attr("value"));
+    user.questionAnswers.push($("#question-range").val());
+    console.log($("#question-range").val());
+    console.log(user.questionAnswers);
+    for(var i = 0; i < user.questionAnswers.length; i++){
+      console.log(user.questionAnswers[i]);
+    }
+  });
+
 });
   //drop down
   $('.dropdown-trigger').dropdown();
