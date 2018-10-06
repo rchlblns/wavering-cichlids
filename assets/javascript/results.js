0707b6338458b8b4506169004393028341f608f5
MattsPoche <MatthewsPoche@comcast.net>
2018-10-06 11:44:09 -0500

    hotel query

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 
         /* !!!!!start travel API!!!! */
         //query for list of hotels
-        /*var travelURL = `https://api.sandbox.amadeus.com/v1.2/hotels/search-circle?apikey=nG40G2MNyhpYFWNBKWFpW83hKIUnrkHO&latitude=${addLat}&longitude=${addLng}&radius=42&check_in=2018-12-15&check_out=2018-12-16`;
+        var travelURL = `https://api.sandbox.amadeus.com/v1.2/hotels/search-circle?apikey=nG40G2MNyhpYFWNBKWFpW83hKIUnrkHO&latitude=${addLat}&longitude=${addLng}&radius=42&check_in=2018-12-15&check_out=2018-12-16`;
         $.ajax({
             url: travelURL,
             method: "GET"
@@ -35,10 +35,8 @@ $(document).ready(function() {
             hotels = response.results;
             console.log("HOTELS:::");
             console.log(hotels);
-            localStorage.setItem("hotels", JSON.stringify(hotels));
             console.log(window.location);
-            window.location.href= "results.html";
-        });*/
+        });
     /* !!!! Contintue Google Maps API !!!! */
     }).then(function() {
         console.log({lat: addLat, lng: addLng});
